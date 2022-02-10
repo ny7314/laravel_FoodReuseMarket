e76df0249fc35273cfd5e4196681a2fd1841b03f
yagunori <92207170+yagunori@users.noreply.github.com>
2022-02-10 12:05:52 +0900

    product store

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,9 +9,25 @@ use App\Models\SecondaryCategory;
 use App\Models\Image;
 use App\Models\Stock;
 
+
+
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'shop_id',
+        'name',
+        'information',
+        'price',
+        'is_selling',
+        'sort_order',
+        'secondary_category_id',
+        'image1',
+        'image2',
+        'image3',
+        'image4',
+    ];
 
     public function shop(){
         return $this->belongsTo(Shop::class);
