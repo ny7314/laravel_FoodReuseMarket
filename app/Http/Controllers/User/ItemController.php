088852f9852ccccead2cc480f083cf5b84475038
yagunori <92207170+yagunori@users.noreply.github.com>
2022-02-12 22:29:36 +0900

    検索フォーム キーワード

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -35,6 +35,7 @@ class ItemController extends Controller
         ->get();
         $products = Product::availableItems()
         ->selectCategory($request->category ?? '0')
+        ->searchKeyword($request->keyword)
         ->sortOrder($request->sort)
         ->paginate($request->pagination ?? '20');
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -109,4 +109,24 @@ class Product extends Model
             return;
         }
     }
+
+    public function scopeSearchKeyword($query, $keyword)
+    {
+        if(!is_null($keyword))
+        {
+            // 全角を半角に変換
+            $spaceConvert = mb_convert_kana($keyword, 's');
+            // 空白で区切る
+            $keywords = preg_split('/[\s]+/', $spaceConvert, -1, PREG_SPLIT_NO_EMPTY);
+            // 単語をループで回す
+            foreach($keywords as $word)
+            {
+                $query->where('products.name', 'like', '%'.$word.'%');
+            }
+            return $query;
+            
+        } else {
+            return;
+        }
+    }
 }
